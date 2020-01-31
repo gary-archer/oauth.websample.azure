a583dab1952f48239dd158b1fd5b8f877e8816da
Gary Archer <Gary.Archer@collinsongroup.com>
2020-01-31 15:03:28 +0000

    Updates

--- api/src/host/utilities/typings.d.ts
+++ api/src/host/utilities/typings.d.ts
@@ -1,7 +1,6 @@
 /*
  * Prevent typescript compile errors for libraries without TypeScript support
  */
-declare module 'got';
 declare module 'js-sha256';
 declare module 'jwk-to-pem';
 declare module 'tunnel-agent';
