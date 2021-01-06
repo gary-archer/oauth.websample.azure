fc7e389edacee5fc16ba078d5b0e1c6e5c4be605
gary <gary@MACSTATION.local>
2021-01-06 18:20:59 +0000

    Initial commit

---
+++ api/src/host/claims/customClaimsProvider.ts
@@ -0,0 +1,9 @@
+import {Request} from 'express';
+import {ApiClaims} from '../../logic/entities/apiClaims';
+
+/*
+ * An interface for providing custom claims that the business logic can implement
+ */
+export interface CustomClaimsProvider {
+    addCustomClaims(accessToken: string, request: Request, claims: ApiClaims): Promise<void>;
+}
---
+++ api/src/host/utilities/typings.d.ts
@@ -0,0 +1,6 @@
+/*
+ * Prevent typescript compile errors for libraries without TypeScript support
+ */
+declare module 'js-sha256';
+declare module 'jwk-to-pem';
+declare module 'tunnel-agent';
