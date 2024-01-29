db6b896dc63384b3929251b8180e69d911291385
gary <gary.archer36@gmail.com>
2024-01-29 19:15:16 +0000

    Updates

--- api/src/logic/entities/claims/userInfoClaims.ts
+++ api/src/logic/entities/claims/userInfoClaims.ts
@@ -5,12 +5,10 @@ export class UserInfoClaims {
 
     private _givenName: string;
     private _familyName: string;
-    private _email: string;
 
-    public constructor(givenName: string, familyName: string, email: string) {
+    public constructor(givenName: string, familyName: string) {
         this._givenName = givenName;
         this._familyName = familyName;
-        this._email = email;
     }
 
     public get givenName(): string {
@@ -19,9 +17,5 @@ export class UserInfoClaims {
 
     public get familyName(): string {
         return this._familyName;
-    }
-
-    public get email(): string {
-        return this._email;
     }
 }
